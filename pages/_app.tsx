ec41b506662ab63f6b274a30af040146625da5b6
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-07-26 17:27:57 +0100

    added landing page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import type { AppProps } from 'next/app'
 import MainLayout from '../components/layout/MainLayout'
-
+import { useRouter } from 'next/router'
 import '../styles/base.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <MainLayout>
-    <Component {...pageProps} />
-  </MainLayout>
+  const router = useRouter();
+  return (
+      <>
+        {
+          router.pathname === '/' ?   <Component {...pageProps} /> :  <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+        }
+      </> 
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,21 @@
-import Link from 'next/link'
-import PostCard from '../components/cards/postCard'
+import React from 'react';
+import { useRouter } from 'next/router';
 
-const IndexPage = () => (
- <div className="p-4 md:p-8">
-  <h1 className="md:ml-6 hidden md:block  mb-3 text-[#6D7175] text-2xl font-semibold font-Poppins ">Latest Posts</h1>
-   <div className='grid md:grid-cols-2 grid-cols-1 '>
-    <PostCard name='Francisca Emmanuel' username='pascal@gmail.com' img="/imgs/user.png" postImg='/imgs/cardFour.png' content='Eating with lovelies' />
-    <PostCard name='Monanu Ifenna' username='pascal@gmail.com' img="/imgs/user.png" postImg='/imgs/cardThree.png' content='Eating with lovelies' />
-    <PostCard name='Francisca Emmanuel' username='pascal@gmail.com' img="/imgs/user.png" postImg='/imgs/cardTwo.png' content='Eating with lovelies' />
-    <PostCard name='Francisca Emmanuel' username='pascal@gmail.com' img="/imgs/cardFive.png" postImg='/imgs/cardOne.png' content='Eating with lovelies' />
-    <PostCard name='Francisca Emmanuel' username='pascal@gmail.com' img="/imgs/user.png" postImg='/imgs/cardFour.png' content='Eating with lovelies' />
-    <PostCard name='Monanu Ifenna' username='pascal@gmail.com' img="/imgs/user.png" postImg='/imgs/cardThree.png' content='Eating with lovelies' />
-    <PostCard name='Francisca Emmanuel' username='pascal@gmail.com' img="/imgs/user.png" postImg='/imgs/cardTwo.png' content='Eating with lovelies' />
-    <PostCard name='Francisca Emmanuel' username='pascal@gmail.com' img="/imgs/user.png" postImg='/imgs/cardOne.png' content='Eating with lovelies' />
-    <PostCard name='Francisca Emmanuel' username='pascal@gmail.com' img="/imgs/user.png" postImg='/imgs/cardFour.png' content='Eating with lovelies' />
-    <PostCard name='Monanu Ifenna' username='pascal@gmail.com' img="/imgs/user.png" postImg='/imgs/cardThree.png' content='Eating with lovelies' />
-    <PostCard name='Francisca Emmanuel' username='pascal@gmail.com' img="/imgs/user.png" postImg='/imgs/cardTwo.png' content='Eating with lovelies' />
-    {/* <PostCard name='Francisca Emmanuel' username='pascal@gmail.com' img="/imgs/user.png" postImg='/imgs/cardOne.png' content='Eating with lovelies' /> */}
-  </div>
- </div>
-)
+function Index() {
+  const router = useRouter();
+  return (
+    <div className='background justify-center relative flex items-center  bg-center bg-cover w-screen h-screen'>
+        <div className="max-w-[600px] h-full absolute bottom-0 md:bottom-auto w-full  shadow-xl opacity-70 rounded-t-[40px] max-h-[320px] md:max-h-[400px] bg-white md:rounded-[20px] flex flex-col items-center justify-center">
+        <div className='flex items-center justify-center  mx-auto w-[150px] flex-col'>
+            <img src="/imgs/logo.png" alt="logo" className='w-full object-center' />
+        </div>
+        <span className="font-Poppins my-2 text-lg">Helping you share the moment...</span>
+        <div className='my-2 flex items-center max-w-[250px] w-full cursor-pointer'>
+            <button className="w-full font-semibold font-Poppins text-gray-600  flex items-center justify-center bg-white border border-[#D1D1D1] py-3  rounded-3xl" onClick={() => router.push('/dashboard')}><img src="/imgs/google.png" /><span className="ml-3">Signin with Google</span></button>
+          </div>
+        </div>
+    </div>
+  )
+}
 
-export default IndexPage
+export default Index
